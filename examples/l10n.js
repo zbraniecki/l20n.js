c6f760b0e76e7bfbcd09406cbb8bd696cbeb65de
Zibi Braniecki <gandalf@mozilla.com>
2014-10-15 13:28:45 +0200

    use immutable ctx/env

--- examples/l10n.js
+++ examples/l10n.js
@@ -1,6 +1,80 @@
 
+ // ENV
+
+function Env(manifest) {
+  this.manifest = manifest;
+}
+
+Env.prototype.createContext = function(langs, resources) {
+  var ctx = new Context(
+    getSupportedLanguages(this.manifest, langs),
+    resources);
+
+  return ctx;
+}
+
+function getSupportedLanguages(manifest, langs) {
+  return getAvailableLanguages(manifest).then(function(manifest) {
+      return Intl.prioritizeLocales(manifest.default_locale,
+                                    manifest.locales,
+                                    langs);
+  });
+}
+
+function getAvailableLanguages(manifest) {
+  return document.l10n.registerLanguages(manifest);
+}
+
+ // Context
+
+function Context(supportedLanguages, resources) {
+  this.ready = supportedLanguages.then(this.fetchResources.bind(this));
+  this.resources = resources;
+}
+
+Context.prototype.fetchResources = function(supported) {
+  return Promise.all(this.resources.map(function(resId) {
+    return navigator.l10n.getResource(supported[0], resId);
+  })).then(function() {
+    return supported;
+  });
+}
+
+Context.prototype.formatEntity = function(l10nId) {
+  return this.ready.then(function(supported) {
+    var lang = supported[0];
+    for (var i = 0; i < this.resources.length; i++) {
+      var resId = this.resources[i];
+      var res = navigator.l10n.resources[resId][lang];
+
+      if (l10nId in res) {
+        return {
+          value: res[l10nId]
+        };
+      }
+    }
+  }.bind(this));
+}
+
+ // BINDINGS
+
+var defaultCtx;
+
+var env = new Env(document.l10n.manifest);
+
+defaultCtx = env.createContext(navigator.languages, document.l10n.resources);
+defaultCtx.ready.then(translateDocument);
+
 document.body.onL10nAttrs(function(nodes) {
-  var f = document.createDocumentFragment();
-  f.textContent = 'foo';
-  nodes[0].setShadowL10n(f);
+  var node = nodes[0];
+  var l10nId = node.getAttribute('l10n-id');
+
+  defaultCtx.formatEntity(l10nId).then(function(entity) {
+    var f = document.createDocumentFragment();
+    f.textContent = entity.value;
+    node.setShadowL10n(f);
+  });
 });
+
+function translateDocument() {
+}
--- examples/webl10n.js
+++ examples/webl10n.js
@@ -1,4 +1,6 @@
 
+// API
+
 Element.prototype.onL10nAttrs = function(cb) {
   if (!(this in moList)) {
     moList[this] = new MutationObserver(onMutations.bind(this, cb));
@@ -60,7 +62,37 @@ function localizeMutations(cb, mutations) {
 
 ///// HEAD
 
-var headObserver = new MutationObserver(onHeadMutations);
+// API
+
+HTMLDocument.prototype.l10n = {
+  manifest: new Promise(function(resolve, reject) {
+    document.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
+      document.removeEventListener('DOMContentLoaded', onDOMContentLoaded);
+      if (l10nManifest instanceof Promise) {
+        l10nManifest.then(function(manifest) {
+          resolve(manifest);
+        });
+      } else {
+        resolve(l10nManifest);
+      }
+    });
+  }),
+
+  registerLanguages: function(manifest) {
+    return new Promise(function(resolve, reject) {
+      resolve(manifest);
+    });
+  },
+
+  resources: [],
+};
+
+//// INTERNAL
+
+var l10nManifest = {
+  default_locale: null,
+  locales: []
+};
 
 var headMOConfig = {
   attributes: false,
@@ -69,31 +101,103 @@ var headMOConfig = {
   subtree: true,
 };
 
+var headObserver = new MutationObserver(onHeadMutations);
+headObserver.observe(document.head, headMOConfig);
+
 function onHeadMutations(mutations, self) {
   var mutation;
 
   for (var i = 0; i < mutations.length; i++) {
     mutation = mutations[i];
-    /*if (mutation.type === 'childList') {
+    if (mutation.type === 'childList') {
       var addedNode;
 
       for (var j = 0; j < mutation.addedNodes.length; j++) {
         addedNode = mutation.addedNodes[j];
 
-        if (addedNode.nodeType !== Node.ELEMENT_NODE) {
-          continue;
-        }
-
-        if (addedNode.childElementCount) {
-          //translateFragment.call(this, addedNode);
-        } else if (addedNode.hasAttribute('data-l10n-id')) {
-          //translateElement.call(this, addedNode);
+        if (addedNode instanceof HTMLMetaElement) {
+          switch (addedNode.getAttribute('name')) {
+            case 'locales':
+              l10nManifest.locales =
+                addedNode.getAttribute('content').split(',').map(
+                  Function.prototype.call, String.prototype.trim);
+              break;
+            case 'default_locale':
+              l10nManifest.default_locale = addedNode.getAttribute('content');
+              break;
+          }
+        } else if (addedNode instanceof HTMLLinkElement) {
+          switch (addedNode.getAttribute('rel')) {
+            case 'manifest':
+              l10nManifest = loadJSON(addedNode.getAttribute('href')).then(
+                function(json) {
+                  var manifest = {};
+                  manifest.locales = json.locales;
+                  manifest.default_locale = json.default_locale;
+                  return manifest;
+              });
+              break;
+            case 'localization':
+              document.l10n.resources.push(addedNode.getAttribute('href'));
+              break;
+          }
         }
       }
-    }*/
-
-    if (mutation.type === 'attributes') {
-      cb([mutation.target]);
     }
   }
+}
+
+/// Intl
+
+Intl.prioritizeLocales = function(availableLocales,
+                                  requestedLocales,
+                                  defaultLocale) {
+  return ['en-US', 'pl'];
+}
+
+// navigator.l10n
+
+var hardDrive = {
+  'locales/example.en-US.properties': {hello: 'Hello'}
+};
+
+Navigator.prototype.l10n = {
+  getResource: function(lang, uri) {
+    return new Promise(function(resolve, reject) {
+      var url = uri.replace('{locale}', lang);
+
+      if (!navigator.l10n.resources[uri]) {
+        navigator.l10n.resources[uri] = {};
+      }
+      navigator.l10n.resources[uri][lang] = hardDrive[url];
+      resolve(hardDrive[url]);
+    });
+  },
+  resources: {},
+};
+
+/// HELPERS
+
+function loadJSON(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+
+    if (xhr.overrideMimeType) {
+      xhr.overrideMimeType('application/json');
+    }
+
+    xhr.open('GET', url);
+
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function io_loadjson(e) {
+      if (e.target.status === 200 || e.target.status === 0) {
+        resolve(e.target.response);
+      } else {
+        reject(e);
+      }
+    });
+    xhr.addEventListener('error', reject);
+    xhr.addEventListener('timeout', reject);
+    xhr.send(null);
+  });
 }
