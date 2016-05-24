2e38e0ec1f286870c0e3f3911c50c8dd088750a4
Zibi Braniecki <gandalf@mozilla.com>
2016-05-23 18:28:49 -0700

    Move createContext to runtime

--- src/bindings/html.js
+++ src/bindings/html.js
@@ -8,16 +8,14 @@ import { initMutationObserver, translateRoots, observe, disconnect }
 import { setAttributes, getAttributes, translateFragment }
   from './dom';
 
-Components.utils.import('resource://gre/modules/Services.jsm');
-
 const properties = new WeakMap();
 const contexts = new WeakMap();
 
 export class Localization {
-  constructor(doc, requestBundles) {
+  constructor(doc, requestBundles, createContext) {
     this.interactive = requestBundles();
     this.ready = this.interactive
-      .then(bundles => fetchFirstBundle(bundles))
+      .then(bundles => fetchFirstBundle(bundles, createContext))
       .then(bundles => translateDocument(this, bundles));
 
     properties.set(this, { doc, requestBundles, ready: false });
@@ -74,37 +72,18 @@ export class Localization {
 Localization.prototype.setAttributes = setAttributes;
 Localization.prototype.getAttributes = getAttributes;
 
-const functions = {
-  OS: function() {
-    switch (Services.appinfo.OS) {
-      case 'WINNT':
-        return 'win';
-      case 'Linux':
-        return 'lin';
-      case 'Darwin':
-        return 'mac';
-      case 'Android':
-        return 'android';
-      default:
-        return 'other';
-    }
-  }
-};
-
-function createContextFromBundle(bundle) {
+function createContextFromBundle(bundle, createContext) {
   return bundle.fetch().then(resources => {
-    const ctx = new Intl.MessageContext(bundle.lang, {
-      functions
-    });
+    const ctx = createContext(bundle.lang);
     resources.forEach(res => ctx.addMessages(res));
     contexts.set(bundle, ctx);
     return ctx;
   });
 }
 
-function fetchFirstBundle(bundles) {
+function fetchFirstBundle(bundles, createContext) {
   const [bundle] = bundles;
-  return createContextFromBundle(bundle).then(
+  return createContextFromBundle(bundle, createContext).then(
     () => bundles
   );
 }
--- src/runtime/gecko/l20n-xul.js
+++ src/runtime/gecko/l20n-xul.js
@@ -3,11 +3,29 @@ import { Localization } from '../../bindings/html';
 import { documentReady, getXULResourceLinks, getXULMeta } from './util';
 import { keysFromContext, valueFromContext } from '../../lib/format';
 
+Components.utils.import('resource://gre/modules/Services.jsm');
 Components.utils.import('resource://gre/modules/L20n.jsm');
 Components.utils.import('resource://gre/modules/IntlMessageContext.jsm');
 Components.utils.import('resource://gre/modules/IntlListFormat.jsm');
 Components.utils.import('resource://gre/modules/IntlPluralRules.jsm');
 Components.utils.import('resource://gre/modules/IntlRelativeTimeFormat.jsm');
+
+const functions = {
+  OS: function() {
+    switch (Services.appinfo.OS) {
+      case 'WINNT':
+        return 'win';
+      case 'Linux':
+        return 'lin';
+      case 'Darwin':
+        return 'mac';
+      case 'Android':
+        return 'android';
+      default:
+        return 'other';
+    }
+  }
+};
 
 function requestBundles(requestedLangs = navigator.languages) {
   return documentReady().then(() => {
@@ -24,12 +42,16 @@ function requestBundles(requestedLangs = navigator.languages) {
   });
 }
 
+function createContext(lang) {
+  return new MessageContext(lang, { functions });
+}
+
 Intl.MessageContext = MessageContext;
 Intl.PluralRules = PluralRules;
 Intl.ListFormat = ListFormat;
 Intl.RelativeTimeFormat = RelativeTimeFormat;
 
-document.l10n = new Localization(document, requestBundles);
+document.l10n = new Localization(document, requestBundles, createContext);
 
 document.l10n.interactive.then(bundles => {
   document.l10n.getValue = function(id, args) {
